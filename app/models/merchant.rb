1d13c9ebc9d67fb4746aed440ee54503c1569d47
Tyler Montgomery <tyler.a.montgomery@gmail.com>
2012-04-21 17:31:55 -0600

    Fixed typo.

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -4,7 +4,7 @@ class Merchant < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
   # For now the merchant can have one address b/c the data
   # does not dictate that merchants can have multiple locations.
-  # We wouls probably want a MerchantLocation model to support
+  # We would probably want a MerchantLocation model to support
   # merchants with more than one location. That would also help
   # us find locations near a potenial Purchaser.
   validates :address, presence: true, uniqueness: true
