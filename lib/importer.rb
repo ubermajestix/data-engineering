cb1c92e75716754912b2347a0f0d105c3a095e30
Tyler Montgomery <tyler.a.montgomery@gmail.com>
2012-04-21 17:56:58 -0600

    Parsing data out of the import file into hashes.

--- lib/importer.rb
+++ lib/importer.rb
@@ -9,6 +9,19 @@
 # Thus, I will use Resque to process the import tasks in the background.
 class Importer
   @queue = "importer"
-  def self.perform
+  def self.perform(upload_id)
+    upload = Upload.find(upload_id)
+    process_csv(upload.import_data_file_name)
+  end
+
+  def self.process_csv(path_to_import_data)
+    CSV.foreach(path_to_import_data, col_sep: "\t", headers: true) do |row|
+      # parse the data
+      merchant_data  = {name: row["merchant_name"], address: ["merchant address"]}
+      item_data      = {description: row["item description"], price: row["item price"]}
+      purchaser_data = {name: row["purchaser name"]}
+      purchase_data  = {count: row["purchase count"]}
+      
+    end
   end
 end
