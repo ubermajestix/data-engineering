095b5885a121ce54722e698b8ee17418d4a3b0ee
Tyler Montgomery <tyler.a.montgomery@gmail.com>
2012-04-22 22:54:21 -0600

    We'll process the data async after its saved.

--- app/controllers/subsidiary_data_controller.rb
+++ app/controllers/subsidiary_data_controller.rb
@@ -11,6 +11,7 @@ class SubsidiaryDataController < ApplicationController
   def create
     @subsidiary_data = SubsidiaryData.new(params[:subsidiary_data])
     if @subsidiary_data.save
+      @subsidiary_data.async_process_import_data
       redirect_to :index
     else
       render :new
