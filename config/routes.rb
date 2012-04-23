545d0e536e7b3b0d6ba6e2518ce52162519089f2
Tyler Montgomery <tyler.a.montgomery@gmail.com>
2012-04-22 22:53:39 -0600

    Added root route. Removed generated index.html.

--- config/routes.rb
+++ config/routes.rb
@@ -48,7 +48,7 @@ DataEngineering::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   root :to => 'subsidiary_data#index'
 
   # See how all your routes lay out with "rake routes"
 
